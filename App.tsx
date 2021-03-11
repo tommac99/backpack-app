52d75f804f62971b59b2cc7837a2bccd14e11dd7
Thomas MacFarlaine <tommacfarlaine@gmail.com>
2021-03-11 12:00:52 +0200

    Add native-base theme provider and theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleProvider } from "native-base";
+import commonColor from "./native-base-theme/variables/commonColor";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import getTheme from "./native-base-theme/components";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,7 +19,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <StyleProvider style={getTheme(commonColor as any)}>
+          <Navigation colorScheme={colorScheme} />
+        </StyleProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
